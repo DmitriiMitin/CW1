7452c028790cbc66daf8fc61fdb44d252048b997
Дмитрий Митин <DVMitin@stud.kpfu.ru>
2022-03-17 11:50:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/CustomDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class CustomDictionary
+    {
+        private Pair[] pairs;
+        public CustomDictionary(String filenames)
+        {
+
+        }
+
+        public CustomDictionary(Pair[] pairs)
+        {
+            this.pairs = pairs;
+        }
+
+        public void Show()
+        {
+            foreach (var p in pairs)
+            {
+                Console.WriteLine(p.Key + " - " + p.Value);
+            }
+        }
+    }
+
+    public class Pair
+    {
+        public string Key { get; set; }
+        public string Value;
+        public Pair(string key, string val)
+        {
+            Key = key;
+            Value = val;
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pairs = new Pair[5];
+            pairs[0] = new Pair("Дом", "House");
+            pairs[1] = new Pair("Человек", "Human");
+            pairs[2] = new Pair("Воздух", "Air");
+            pairs[3] = new Pair("Камень", "Rock");
+            pairs[4] = new Pair("Трава", "Grass");
+
+
+            var customDictionary = new CustomDictionary(pairs);
+            customDictionary.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
